8e68e4c85f21be51f48635dff7a7ddfde6de3a8e
Omar Babaverdiyev <101584938+OmarBbv@users.noreply.github.com>
2024-08-14 16:48:42 +0400

    Update upload.js

--- api/src/middleware/upload.js
+++ api/src/middleware/upload.js
@@ -2,23 +2,20 @@ const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require('cloudinary').v2;
 
-// Cloudinary yapılandırması
 cloudinary.config({
   cloud_name: 'dwxn9w5be',
   api_key: '233282418171293',
   api_secret: 'iMn6h6rDxTpChgAAOBoSarEEyag',
 });
 
-// Cloudinary Storage yapılandırması
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'uploads', // Resimlerin Cloudinary'de saklanacağı klasör
+    folder: 'uploads', 
     allowedFormats: ['jpg', 'jpeg', 'png', 'gif'],
   },
 });
 
-// Multer yapılandırması
 const upload = multer({ storage }).array("images", 12);
 
 module.exports = upload;
